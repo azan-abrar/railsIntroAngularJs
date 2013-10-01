6e3a9547c4391b7d2d9875e6d133c6f584faeb06
Azan <azan@azan-Vostro-5460.(none)>
2013-10-01 08:00:34 +0500

    structured app

---
+++ app/assets/javascripts/angular/controllers/loginController.js
@@ -0,0 +1,9 @@
+app.controller("LoginController", function($scope, $location, AuthenticationService) {
+  $scope.credentials = {username: "", password: ""};
+
+  $scope.login = function() {
+    AuthenticationService.login($scope.credentials).success(function() {
+      $location.path('/home');
+    });
+  };
+});
---
+++ app/assets/javascripts/angular/services/AuthenticationService.js
@@ -0,0 +1,40 @@
+app.factory("AuthenticationService", function($http, $sanitize, SessionService, FlashService) {
+
+  var cacheSession = function() {
+    SessionService.set('authenticated', true);
+  };
+
+  var uncacheSession = function() {
+    SessionService.unset('authenticated');
+  };
+
+  var loginError = function(response) {
+    FlashService.show(response.flash);
+  };
+
+  var sanitizeCredentials = function(credentials) {
+    return {
+      username: $sanitize(credentials.username),
+      password: $sanitize(credentials.password)
+    };
+  };
+
+  return {
+    login: function(credentials) {
+      var login = $http.post("/auth/signin", sanitizeCredentials(credentials));
+      login.success(cacheSession);
+      login.success(FlashService.clear);
+      login.error(loginError);
+      return login;
+    },
+    logout: function() {
+      var logout = $http.get("/auth/signout");
+      logout.success(uncacheSession);
+      return logout;
+    },
+    isLoggedIn: function() {
+      return SessionService.get('authenticated');
+    }
+  };
+});
+
